f45c876e13462e1f04eaafc25484c16759fae144
ShruthaKashyap <shrutha.kashyap@gmail.com>
2017-10-01 13:17:35 -0400

    Working code: with user-given inputs and few other changes

--- src/Solver.java
+++ src/Solver.java
@@ -1,25 +1,66 @@
+
 import java.util.*;
 
 public class Solver {
 
 	static int size=3; //3X3 board
 	static PriorityQueue<Node> minPQ=new PriorityQueue<Node>();
-	
+	static ArrayList<int[][]> closedList=new ArrayList<int[][]>(); //to keep track of the visited nodes
+	static ArrayList<Node> children=new ArrayList<Node>();
+	static int nodesGenerated=0;
+	static int nodesExpanded=0;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] initial={{1,2,3},{7,5,6},{8,4,0}};
 		
-		int[][] goal={{1,2,3},{4,5,6},{7,8,0}};
+		Scanner sc = new Scanner(System.in);
 		
-		int i_blank=2,j_blank=2;
+		int[][] initial=new int[size][size];
+		int[][] goal=new int[size][size];
 		
-		int moves=solve(initial,i_blank,j_blank,goal);
+		int i_blank = 0,j_blank = 0;
 		
-		System.out.println("Total no of moves needed: "+moves);
+		System.out.println("Enter values for initial state:");
+		
+		for (int i = 0; i < size; i++)
+		{
+		    for(int j = 0; j < size; j++) {
+		        System.out.println("Row ["+i+"]:  Column ["+j+"]:");
+		        initial[i][j] = sc.nextInt();
+		    }
+		}
+		
+		System.out.println("Enter values for goal state:");
+		
+		for (int i = 0; i < size; i++)
+		{
+		    for(int j = 0; j < size; j++) {
+		        System.out.println("Row ["+i+"]:  Column ["+j+"]:");
+		        goal[i][j] = sc.nextInt();
+		    }
+		}
+		
+		sc.close();
+		
+		for (int i = 0; i < size; i++)
+		{
+		    for(int j = 0; j < size; j++) {
+		        if(initial[i][j]==0)
+		        {
+		        	i_blank=i;
+		        	j_blank=j;
+		        }
+		    }
+		}
+		
+		solve(initial,i_blank,j_blank,goal);
+		
+		System.out.println("No of nodes generated: "+nodesGenerated);
+		System.out.println("No of nodes expanded: "+nodesExpanded);
 		
 	}
 	
-	static int solve(int[][]initial,int i_blank,int j_blank, int[][] goal)
+	static void solve(int[][]initial,int i_blank,int j_blank, int[][] goal)
 	{
 		
 		Node root=new Node(i_blank,j_blank,initial,null,0);
@@ -28,9 +69,10 @@ public class Solver {
 		
 		root.compute_f_of_n(root.g, root.h);
 		
-		minPQ.add(root);
+		nodesGenerated++;
 		
-		int noOfMoves=0;
+		minPQ.add(root);
+	
 		
 		while(minPQ.isEmpty()==false)
 		{
@@ -45,17 +87,57 @@ public class Solver {
 			}
 			
 			expand(current,goal); //Find the children of this node
-				
-			noOfMoves++;
-		}
-		
-		return noOfMoves;
+			
+			HashMap<int[][],Node> map=new HashMap<int[][],Node>(); //to track duplicate nodes
+			
+			for(Node n:children)
+			{
+				if(!closedList.contains(n.puzzle)) //checks if the state is same as its ancestor
+				{
+					if(minPQ.size()==0)
+					{
+						n.computeManhattanSum(n.puzzle, goal);
+						n.compute_f_of_n(n.g, n.h);
+						minPQ.add(n); 
+						map.put(n.puzzle, n);
+					}
+					
+					else
+					{
+						
+						if(map.containsKey(n.puzzle))
+						{
+							if(map.get(n.puzzle).g>n.g) //avoid repeated states in the queue
+							{
+								minPQ.remove(map.get(n.puzzle)); //remove the node with higher g value
+								n.computeManhattanSum(n.puzzle, goal);
+								n.compute_f_of_n(n.g, n.h);
+								minPQ.add(n);
+								map.put(n.puzzle, n);
+							}
+						}
+						else
+						{
+							n.computeManhattanSum(n.puzzle, goal);
+							n.compute_f_of_n(n.g, n.h);
+							minPQ.add(n);
+							map.put(n.puzzle, n);
+						}
+					}
+				}
+			}
+			
+			children.clear();
+		}	
 	}
 	
 	
 	//4 possible moves or children for a given puzzle
 	static void expand(Node current,int[][] goal)
 	{
+		nodesExpanded++;
+		closedList.add(current.puzzle);
+		
 		// row and column indices to define the downward, backward, upward, forward movements respectively
 		int[] rowMoves={1,0,-1,0};
 		int[] colMoves={0,-1,0,1};
@@ -83,11 +165,8 @@ public class Solver {
 									current,
 									current.g+1,
 									keep);
-				
-				child.computeManhattanSum(child.puzzle, goal);
-				child.compute_f_of_n(child.g, child.h);
-				
-				minPQ.add(child);
+				nodesGenerated++;
+				children.add(child);
 			}
 		}
 	}
@@ -104,6 +183,7 @@ public class Solver {
 	
 	static void printPuzzle(int[][] puzzle)
 	{
+		
 		for(int f=0;f<size;f++)
 		{
 			for(int h=0;h<size;h++)
@@ -116,10 +196,14 @@ public class Solver {
 	
 	static void printPath(Node root)
 	{
+		
 		if (root==null)return;
 	    printPath(root.parent);
 	    printPuzzle(root.puzzle);
-	 
+	    System.out.println("g(n)= "+root.g);
+	    System.out.println("h(n)= "+root.h);
+	    System.out.println("f(n)= "+root.f);
+
 	    System.out.println();
 	}
 }
